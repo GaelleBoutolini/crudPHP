519da78cb0e728dc28a714eafac8a9fad9ef672c
Hanaline <gaelle.boutolini.simplon@gmail.com>
2023-02-08 13:35:21 +0100

    faire la moyenne

--- ajouter.php
+++ ajouter.php
@@ -9,44 +9,20 @@
 </head>
 <body>
     <?php
-    //vérifier que le bouton cliquer a été bien cliqué
-    if(isset($_POST['button'])){
-
-       //extraction des information envoyées dans des variables par la méthode POST
-       extract($_POST);
-
-    //Vérifier que tous les champs ont été bien remplis
-    if(isset($nom) & isset($prenom) & isset($age)){
-
+    
     //connexion à la base de données
     include_once "connexion.php";
     //requete d'ajout
-    $response = $bdd->query("INSERT INTO employe VALUES(NULL, '$nom', '$prenom', '$age')");
-    if($response){//si la requete a été effectuée avec succès, on fait une redirection 
+
         header("location: index.php");
-    }else{
-        $message = "Employe non ajouté";
-    }
 
-    }else{
-        //sinon
         $message = "Vueillez remplir tous les champs !";
-
-    }
-
-    }
 
     ?>
     <div class="form">
         <a href="index.php" class="back_btn"><img src="/images/back.png" alt="">Retour</a>
         <h2>Ajouter un employé</h2>
         <p class="erreur_message">
-            <?php
-            //Si la variable message existe, affichons son contenu
-            if(isset($message)){
-                echo $message;
-            }
-            ?>
             </p>
         <form action="" method="POST">
         <label>Nom</label>
--- supprimer.php
+++ supprimer.php
@@ -1,25 +1,12 @@
 
-
 <?php
- include_once "connexion.php";
-try {
-    $id = (isset($_GET['id']) && empty($_GET['id'])) ? intval($_GET['id']) : 0;
-
-    if ($id > 0) {
-        $pdo = new PDO('mysql:host=localhost;dbname=societe', 'root', '');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $sql = "DELETE FROM employe WHERE id = :id";
-        $id = $pdo->prepare($sql);
-        $id->bindValue(':id', $id, PDO::PARAM_INT);
-        $id->execute();
-
-        echo "L'enregistrement a été supprimé avec succès.";
-    }
-} catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
-    header("location: index.php");
-}
+require_once "connexion.php";
+$id = $_GET['id'];
+//essaie un par un et choiser celui qui marche suis pas sûr
+$bdd->exec("DELETE FROM employe WHERE id=`$id`");
+$bdd->exec("DELETE FROM employe WHERE id=".$id);
+echo "entrées supprimées !";
+header("Location:index.php");
 ?>
 
 
